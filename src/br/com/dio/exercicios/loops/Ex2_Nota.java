f885560e30b33f380baa97bb7a6b236dcb9c025c
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-01 23:55:09 -0300

    update

--- src/br/com/dio/exercicios/loops/Ex2_Nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_Nota.java
@@ -10,8 +10,19 @@ até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int note;
+        System.out.println("\nDigite a primeira nota: ");
+        note = scanner.nextInt();
 
+        while (note < 0 || note > 10) {
+            System.out.println("\nNota " + note + " inválida\n");
+            System.out.println("Digite a nota novamente:");
+            note = scanner.nextInt();
+        }
 
+        System.out.println("\nA nota digitada foi: " + note + ".");
+        System.out.println("\nAté a próxima.");
     }
 
 }
