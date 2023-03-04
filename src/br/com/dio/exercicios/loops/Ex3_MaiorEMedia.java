0921705f1629c87dac27aa46478adfd7a4709151
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-04 07:29:01 -0300

    I changed the type of the variable from double to int

--- src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -9,7 +9,19 @@ e a média desses números.
 */
 public class Ex3_MaiorEMedia {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int greater = 0;
 
+        int count = 0;
+        do {
+            System.out.println("Digito um número inteiro: ");
+            number = scanner.nextInt();
+            if (number > greater) greater = number;
+            count++;
+        } while (count < 5);
 
+        System.out.println("O maior número dentre os cinco digitados é : " + greater);
+        System.out.println("\nAté a próxima!");
     }
 }
