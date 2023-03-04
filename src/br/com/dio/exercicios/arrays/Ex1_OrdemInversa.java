c48896d38aeb934987542814603dd937ec48b92a
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-04 10:22:03 -0300

    code to generate an inverse vector created.

--- src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
+++ src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -6,6 +6,18 @@ e mostre-os na ordem inversa.
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
 
+        int[] vector = { -5, 0, 15, 20, 8, 4 };
 
+        System.out.println("\nVetor: ");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
+        }
+
+        System.out.println("\n\nVetor inverso: ");
+        for (int i = (vector.length - 1); i >= 0; i--) {
+            System.out.print(vector[i] + " ");
+        }
+
+        System.out.println("\n\nFim do programa!");
     }
 }
