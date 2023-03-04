257acdb2029319fb1baa9b601d8b5579e731c6b8
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-04 10:48:39 -0300

    code to generate random numbers created

--- src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
+++ src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
@@ -8,8 +8,30 @@ Ao final mostre os números e seus sucessores.
 */
 public class Ex3_NumerosAleatorios {
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int[] randomNumbers = new int[20];
+        for (int i = 0; i < randomNumbers.length; i++) {
+            int number = random.nextInt(100);
+            randomNumbers[i] = number;
+        }
 
+        System.out.println("\nNúmeros aleatórios: ");
+        for (int randomNumber : randomNumbers) {
+            System.out.print(randomNumber + " ");
+        }
+
+        System.out.println("\n\nSucessores dos números aleatórios acima: ");
+        for (int randomNumber : randomNumbers) {
+            System.out.print((randomNumber + 1) + " ");
+        }
+
+        System.out.println("\n\nAntecessores dos números aleatórios acima: ");
+        for (int randomNumber : randomNumbers) {
+            System.out.print((randomNumber - 1) + " ");
+        }
+
+        System.out.println("\n\nFim do progama!");
     }
 
 }
