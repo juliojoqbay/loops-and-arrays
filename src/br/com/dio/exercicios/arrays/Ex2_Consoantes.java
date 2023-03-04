5cfd07c6222379fa0271add564f934703b253e3b
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-04 10:23:08 -0300

    code to capture consonant letters created.

--- src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
+++ src/br/com/dio/exercicios/arrays/Ex2_Consoantes.java
@@ -9,8 +9,37 @@ import java.util.Scanner;
 
 public class Ex2_Consoantes {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] consonants = new String[6];
+        int consonantsQuantity = 0;
 
 
+        System.out.println("\nVamos averiguar se uma letra é consoante.");
+        System.out.println("\nDigite 6 letras no total:");
+        System.out.println("\nVamos começar!");
+        int count = 0;
+        do {;
+            System.out.println("\nLetra: ");
+            String letter = scanner.nextLine();
+
+            if (!(letter.equalsIgnoreCase("a") ||
+                    letter.equalsIgnoreCase("e") ||
+                    letter.equalsIgnoreCase("i") ||
+                    letter.equalsIgnoreCase("o") ||
+                    letter.equalsIgnoreCase("u"))) {
+                consonants[count] = letter;
+                consonantsQuantity++;
+            }
+            count++;
+        } while (count < consonants.length);
+
+        System.out.println("\nConsoantes: ");
+        for (String consonante : consonants) {
+            if (consonante != null)
+                System.out.println("\n" + consonante);
+        }
+
+        System.out.println("\nQuantidade de consoantes: " + consonantsQuantity + ".");
     }
 
 }
