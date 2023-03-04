77b304ae121ac06f96f3941d8d7e97696cec9c5f
juliojoqbay <202301537614@alunos.estacio.br>
2023-03-04 09:25:39 -0300

    code to generate factorial calculation

--- src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
+++ src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
@@ -1,5 +1,6 @@
 package br.com.dio.exercicios.loops;
 
+import javax.xml.stream.FactoryConfigurationError;
 import java.util.Scanner;
 
 /*
@@ -8,7 +9,18 @@ Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 */
 public class Ex6_Fatorial {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("\nDigite um número para cálculo fatorial:\n ");
+        int factorial = scanner.nextInt();
+
+        int multiplication = 1;
+
+        System.out.print(factorial + "! = ");
+        for (int i = factorial; i >= 1; i--) {
+            multiplication *= i;
+        }
+        System.out.println(multiplication);
     }
 
 }
